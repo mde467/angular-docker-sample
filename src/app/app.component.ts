30495585365b66d3a909312c1812389147230476
Mrudula <mdeokute@gmail.com>
2020-11-01 15:53:55 +0530

    2.Feature commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dockerized the angular application';
+  title = 'Dockerized the angular application - sample';
 }
